7d570eb65bdc610b05b955dfd35a08d061f283c9
Raquib Khan Lavani <66289290+raquibk@users.noreply.github.com>
2021-06-15 16:45:04 -0600

    Handles queries with multiple words

--- back_end/server/scripts/walmartscraper.py
+++ back_end/server/scripts/walmartscraper.py
@@ -7,8 +7,35 @@ browser = webdriver.Chrome('D:\DOWNLOADS\Setups\ChromeDriver\chromedriver.exe')
 
 
 item_name = input('What do you want to search for? ')
-item_url = 'https://www.walmart.ca/search?q=' + item_name
+item_name_words = []
+word = ""
+count = 0
+item_url = 'https://www.walmart.ca/search?q='
 
+for letter in item_name:
+    count+=1
+    if letter == " ":
+        item_name_words.append(word)
+        word = ""
+
+    elif count == len(item_name):
+        word+=item_name[-1]
+        item_name_words.append(word)
+
+    else:
+        word+=letter
+
+if len(item_name_words) == 1:
+    item_url+=item_name
+
+else:
+    for i in range(len(item_name_words)-1):
+        item_url+=item_name_words[i]
+        item_url+= '%20'
+    
+    item_url+=item_name_words[-1]
+
+print(item_url)
 browser.get(item_url)
 item_list = browser.find_elements_by_css_selector('span.css-2vqe5n.esdkp3p0')
 
