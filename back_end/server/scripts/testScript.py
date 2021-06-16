54e96c77e51a32e6a4f79b0f26a75e83a8db346c
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-06-16 00:30:04 -0600

    Added backend query functionality

--- back_end/server/scripts/testScript.py
+++ back_end/server/scripts/testScript.py
@@ -1,3 +1,4 @@
+import json
 from superstoreAgent import get_prices
 
 items = [
@@ -12,5 +13,23 @@ def check_superstore():
   print("****Superstore Prices***")
   get_prices(items)
 
+def load_items():
+  '''
+  Loads items from the json file
+  :param None
+  :returns None
+  '''
+  global items
+  # Read the json file
+  try:
+    with open('./data/query.json') as infile:
+      data = json.load(infile)
+      items = data['items']
+  except Exception as e:
+    print("[Exception]", e)
+  
+
+
 if __name__ == '__main__':
+  load_items()
   check_superstore()
