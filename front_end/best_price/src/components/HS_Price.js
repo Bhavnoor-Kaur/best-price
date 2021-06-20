459805bfdc4bfd7537542a789a4499d7dc3cf67a
Vardan Saini <Vardan1@ualberta.ca>
2021-06-20 04:25:01 -0600

    Minor fixes

--- front_end/best_price/src/components/HS_Price.js
+++ front_end/best_price/src/components/HS_Price.js
@@ -2,8 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import PricingBox from "./PricingBox";
 
-import './HS_Price.css'
-
 function HS_Price() {
     return (
         <>
