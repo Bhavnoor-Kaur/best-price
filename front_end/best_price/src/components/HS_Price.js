0eb72206824f7fb3fa625bd02c3d45e7ff70f6b5
Vardan Saini <Vardan1@ualberta.ca>
2021-06-20 02:53:21 -0600

    Prising Design Added

---
+++ front_end/best_price/src/components/HS_Price.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import PricingBox from "./PricingBox";
+import { Button } from './Button'
+import './HS_Price.css'
+
+function HS_Price() {
+    return (
+        <>
+         <PricingBox />
+        </>
+    )
+}
+
+export default HS_Price
--- front_end/best_price/src/components/pages/About.js
+++ front_end/best_price/src/components/pages/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../App.css'
 import HS_list from '../HS_list'
+import HS_Price from '../HS_Price'
 import Footer from '../Footer'
 
 export default function About() {
     return(
         <>
         <HS_list />
+        <HS_Price />
         <Footer />
         </>
 
